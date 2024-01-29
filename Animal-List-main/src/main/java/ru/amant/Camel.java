a6a8a5d4e76a1421564c6856ebf7be8f701fb0d1
Aleksandr Belyi <127232280+Amant-987@users.noreply.github.com>
2024-01-29 11:43:12 +0300

    checked files

---
+++ Animal-List-main/src/main/java/ru/amant/Camel.java
@@ -0,0 +1,7 @@
+package ru.amant;
+
+public class Camel extends PackAnimal {
+  public Camel(String name) {
+    super(name);
+  }
+}
---
+++ Animal-List-main/src/main/java/ru/amant/Hamster.java
@@ -0,0 +1,7 @@
+package ru.amant;
+
+public class Hamster extends Pet {
+  public Hamster(String name) {
+    super(name);
+  }
+}
