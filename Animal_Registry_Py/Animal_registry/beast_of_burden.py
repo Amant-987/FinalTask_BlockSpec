60e1b7c686b753739b767216ef84e01564f63064
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2024-01-29 12:34:28 +0300

    added some class

---
+++ Animal_Registry_Py/Animal_registry/beast_of_burden.py
@@ -0,0 +1,6 @@
+from Animal_registry import Animal
+
+
+class BeastOfBurden(Animal):
+    def __init__(self, name):
+        super().__init__(name)
---
+++ Animal_Registry_Py/Animal_registry/pet.py
@@ -0,0 +1,6 @@
+from Animal_registry import Animal
+
+
+class Pet(Animal):
+    def __init__(self, name):
+        super().__init__(name)
